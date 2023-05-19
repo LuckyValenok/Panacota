e7938bb8f00f231329bd34e84b42ab1cf20d8c00
Vlad <luckyvalenok1@gmail.com>
2023-05-19 18:15:04 +0500

    Добавлен SplashScreen и шрифт Didact Gothic

---
+++ app/src/main/java/net/panacota/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package net.panacota.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.main_activity)
+    }
+}
