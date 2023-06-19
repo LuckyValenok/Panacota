4e5dc23d1129cdacd261e403b499ff2d3c1ef4c8
Vlad <luckyvalenok1@gmail.com>
2023-06-19 21:34:06 +0500

    Исправлен CategoryDiffUtil

--- app/src/main/java/net/panacota/app/ui/adapters/CategoriesAdapter.kt
+++ app/src/main/java/net/panacota/app/ui/adapters/CategoriesAdapter.kt
@@ -36,7 +36,7 @@ class CategoriesAdapter(private val onClick: (Category) -> Unit) :
 
     class CategoryDiffUtil : DiffUtil.ItemCallback<Category>() {
         override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean =
-            oldItem == newItem
+            oldItem.type == newItem.type
 
         override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean =
             oldItem == newItem
